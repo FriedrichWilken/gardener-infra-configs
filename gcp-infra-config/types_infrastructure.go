cb2011f220b366a652d54fa82d6df3e1a495c3f5
Friedrich Wilken <jens.wilken@sap.com>
2022-04-27 17:01:32 +0200

    take all of gcp

--- gcp-infra-config/types_infrastructure.go
+++ gcp-infra-config/types_infrastructure.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package gcp_infra_config
+package gcp
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,57 +23,52 @@ import (
 
 // InfrastructureConfig infrastructure configuration resource
 type InfrastructureConfig struct {
-	metav1.TypeMeta `json:",inline"`
+	metav1.TypeMeta
 
 	// Networks is the network configuration (VPC, subnets, etc.)
-	Networks NetworkConfig `json:"networks"`
+	Networks NetworkConfig
 }
 
 // NetworkConfig holds information about the Kubernetes and infrastructure networks.
 type NetworkConfig struct {
 	// VPC indicates whether to use an existing VPC or create a new one.
-	// +optional
-	VPC *VPC `json:"vpc,omitempty"`
+	VPC *VPC
 	// CloudNAT contains configuration about the the CloudNAT resource
-	// +optional
-	CloudNAT *CloudNAT `json:"cloudNAT,omitempty"`
+	CloudNAT *CloudNAT
 	// Internal is a private subnet (used for internal load balancers).
-	// +optional
-	Internal *string `json:"internal,omitempty"`
+	Internal *string
 	// Worker is the worker subnet range to create (used for the VMs).
 	// Deprecated - use `workers` instead.
-	Worker string `json:"worker"`
+	Worker string
 	// Workers is the worker subnet range to create (used for the VMs).
-	Workers string `json:"workers"`
+	Workers string
 	// FlowLogs contains the flow log configuration for the subnet.
-	// +optional
-	FlowLogs *FlowLogs `json:"flowLogs,omitempty"`
+	FlowLogs *FlowLogs
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // InfrastructureStatus contains information about created infrastructure resources.
 type InfrastructureStatus struct {
-	metav1.TypeMeta `json:",inline"`
+	metav1.TypeMeta
 
 	// Networks is the status of the networks of the infrastructure.
-	Networks NetworkStatus `json:"networks"`
+	Networks NetworkStatus
 
 	// ServiceAccountEmail is the email address of the service account.
-	ServiceAccountEmail string `json:"serviceAccountEmail"`
+	ServiceAccountEmail string
 }
 
 // NetworkStatus is the current status of the infrastructure networks.
 type NetworkStatus struct {
 	// VPC states the name of the infrastructure VPC.
-	VPC VPC `json:"vpc"`
+	VPC VPC
 
 	// Subnets are the subnets that have been created.
-	Subnets []Subnet `json:"subnets"`
+	Subnets []Subnet
 
 	// NatIPs is a list of all user provided external premium ips which can be used by the nat gateway
-	// +optional
-	NatIPs []NatIP `json:"natIPs,omitempty"`
+	NatIPs []NatIP
 }
 
 // SubnetPurpose is a purpose of a subnet.
@@ -89,58 +84,52 @@ const (
 // Subnet is a subnet that was created.
 type Subnet struct {
 	// Name is the name of the subnet.
-	Name string `json:"name"`
+	Name string
 	// Purpose is the purpose for which the subnet was created.
-	Purpose SubnetPurpose `json:"purpose"`
+	Purpose SubnetPurpose
 }
 
 // VPC contains information about the VPC and some related resources.
 type VPC struct {
 	// Name is the VPC name.
-	Name string `json:"name,omitempty"`
+	Name string
 	// CloudRouter indicates whether to use an existing CloudRouter or create a new one
-	// +optional
-	CloudRouter *CloudRouter `json:"cloudRouter,omitempty"`
+	CloudRouter *CloudRouter
 }
 
 // CloudRouter contains information about the the CloudRouter configuration
 type CloudRouter struct {
 	// Name is the CloudRouter name.
-	Name string `json:"name,omitempty"`
+	Name string
 }
 
 // CloudNAT contains configuration about the the CloudNAT resource
 type CloudNAT struct {
 	// MinPortsPerVM is the minimum number of ports allocated to a VM in the NAT config.
 	// The default value is 2048 ports.
-	// +optional
-	MinPortsPerVM *int32 `json:"minPortsPerVM,omitempty"`
-	// NatIPNames is a list of all user provided external premium ips which can be used by the nat gateway
-	// +optional
-	NatIPNames []NatIPName `json:"natIPNames,omitempty"`
+	MinPortsPerVM *int32
+	// NatIPNames is a list of all names of user provided external premium ips which can be used by the nat gateway
+	NatIPNames []NatIPName
 }
 
 // NatIP is a user provided external ip which can be used by the nat gateway
 type NatIP struct {
 	// IP is the external premium IP address used in GCP
-	IP string `json:"ip"`
+	IP string
 }
 
 // NatIPName is the name of a user provided external ip address which can be used by the nat gateway
 type NatIPName struct {
 	// Name of the external premium ip address which is used in gcp
-	Name string `json:"name"`
+	Name string
 }
 
 // FlowLogs contains the configuration options for the vpc flow logs.
 type FlowLogs struct {
 	// AggregationInterval for collecting flow logs.
-	// +optional
-	AggregationInterval *string `json:"aggregationInterval,omitempty"`
+	AggregationInterval *string
 	// FlowSampling sets the sampling rate of VPC flow logs within the subnetwork where 1.0 means all collected logs are reported and 0.0 means no logs are reported.
-	// +optional
-	FlowSampling *float32 `json:"flowSampling,omitempty"`
+	FlowSampling *float32
 	// Metadata configures whether metadata fields should be added to the reported VPC flow logs.
-	// +optional
-	Metadata *string `json:"metadata,omitempty"`
+	Metadata *string
 }
---
+++ gcp-infra-config/types_worker.go
@@ -0,0 +1,78 @@
+// Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcp
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WorkerConfig contains configuration settings for the worker nodes.
+type WorkerConfig struct {
+	metav1.TypeMeta
+
+	// Volume contains configuration for the root disks attached to VMs.
+	Volume *Volume
+
+	// Service account, with their specified scopes, authorized for this worker.
+	// Service accounts generate access tokens that can be accessed through
+	// the metadata server and used to authenticate applications on the
+	// instance.
+	// This service account should be created in advance.
+	ServiceAccount *ServiceAccount
+}
+
+// Volume contains configuration for the additional disks attached to VMs.
+type Volume struct {
+	// LocalSSDInterface is the interface of that the local ssd disk supports.
+	LocalSSDInterface *string
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WorkerStatus contains information about created worker resources.
+type WorkerStatus struct {
+	metav1.TypeMeta
+
+	// MachineImages is a list of machine images that have been used in this worker. Usually, the extension controller
+	// gets the mapping from name/version to the provider-specific machine image data in its componentconfig. However, if
+	// a version that is still in use gets removed from this componentconfig it cannot reconcile anymore existing `Worker`
+	// resources that are still using this version. Hence, it stores the used versions in the provider status to ensure
+	// reconciliation is possible.
+	MachineImages []MachineImage
+}
+
+// MachineImage is a mapping from logical names and versions to GCP-specific identifiers.
+type MachineImage struct {
+	// Name is the logical name of the machine image.
+	Name string
+	// Version is the logical version of the machine image.
+	Version string
+	// Image is the path to the image.
+	Image string
+}
+
+// ServiceAccount is a GCP service account.
+type ServiceAccount struct {
+	// Email is the email address of the service account.
+	Email string
+
+	// Scopes is the list of scopes to be made available for this service.
+	// account.
+	Scopes []string
+}
