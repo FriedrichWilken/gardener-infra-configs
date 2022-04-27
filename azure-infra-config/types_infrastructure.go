959b52609321e4244a9bf827f69075934b96ad54
Friedrich Wilken <jens.wilken@sap.com>
2022-04-27 10:20:55 +0200

    add infra config for azure

--- azure-infra-config/types_infrastructure.go
+++ azure-infra-config/types_infrastructure.go
@@ -1,1 +1,20 @@
 package azure_infra_config
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type InfrastructureConfig struct {
+	metav1.TypeMeta `json:",inline"`
+	// ResourceGroup is azure resource group.
+	// +optional
+	ResourceGroup *ResourceGroup `json:"resourceGroup,omitempty"`
+	// Networks is the network configuration (VNet, subnets, etc.).
+	Networks NetworkConfig `json:"networks"`
+	// Identity contains configuration for the assigned managed identity.
+	// +optional
+	Identity *IdentityConfig `json:"identity,omitempty"`
+	// Zoned indicates whether the cluster uses availability zones.
+	// +optional
+	Zoned bool `json:"zoned,omitempty"`
+}
