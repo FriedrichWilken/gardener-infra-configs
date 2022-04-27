5f300b8b832da5441bce8ec87a78cec3ee5b645b
Friedrich Wilken <jens.wilken@sap.com>
2022-04-27 10:20:39 +0200

    add infra config for gpc

--- gcp-infra-config/types_infrastructure.go
+++ gcp-infra-config/types_infrastructure.go
@@ -1,1 +1,13 @@
 package gcp_infra_config
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// InfrastructureConfig infrastructure configuration resource
+type InfrastructureConfig struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// Networks is the network configuration (VPC, subnets, etc.)
+	Networks NetworkConfig `json:"networks"`
+}
